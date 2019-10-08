342fdab544d00bf61ad061b1d4066f6b7a3e2cca
pisichi <win.pisichi@gmail.com>
2019-10-08 20:54:16 +0700

    add realpage to branch

---
+++ pages/realpage/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <div class = "col-9">
+     div1
+    </div>
+    <div class = "col-3">
+     div2
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
